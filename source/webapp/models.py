e453bbba1beb2a8e469f9794263e0233d50cf457
Beknasar <680633@gmail.com>
2020-08-02 16:06:05 +0600

    Добавлена валидация на минимальное значение в поля цена и количество в модель товара.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 
 
 DEFAULT_CATEGORY = 'other'
@@ -16,8 +16,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
     category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Категория')
-    amount = models.IntegerField(verbose_name='Остаток')
-    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2)
+    amount = models.IntegerField(verbose_name='Остаток', validators=(MinValueValidator(0),))
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2, validators=(MinValueValidator(0),))
 
     def __str__(self):
         return f'{self.name} -- {self.amount}'
