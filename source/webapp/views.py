dcd758dd6b5bc009f9c036ffbe88a2531427bebd
Beknasar <680633@gmail.com>
2020-08-03 19:12:33 +0600

    Настроены кнопки на странице удаления.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Product
 
+
 from django.http import HttpResponseNotAllowed
 from .forms import ProductForm
 
